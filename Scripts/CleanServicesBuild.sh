a676739d913ad2a133d5a65d107fb8eafa704d69
The-Arkitekt <mr.davidcrumley@gmail.com>
2023-07-21 12:40:14 -0400

    Add in Service Framework clean

--- Scripts/CleanServicesBuild.sh
+++ Scripts/CleanServicesBuild.sh
@@ -2,11 +2,17 @@
 
 MMS_SCRIPTS_DIR=${PWD}/../../MovementManagementSystem/Scripts
 RG_SCRIPTS_DIR=${PWD}/../../RemoteGateway/Scripts
+SF_SCRIPTS_DIR=${PWD}/../../ServiceFramework/Scripts
+EMULATOR_DIR="Emulators"
 
 cd ${MMS_SCRIPTS_DIR}
 /bin/bash CleanMovementManagementSystemBuild.sh ${1}
 
 cd  ${MMS_SCRIPTS_DIR}
 /bin/bash CleanRemoteGatewayBuild.sh ${1}
+
+cd ${SF_SCRIPTS_DIR}
+/bin/bash CleanServiceFrameworkBuild.sh ${1}
+/bin/bash CleanServiceFrameworkBuild.sh ${EMULATOR_DIR}
 
 exit
